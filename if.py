e7b0934374d7f9ad8a52fcc9cbd58bc6c9bb8ca9
rajritika <35676563+rajritika@users.noreply.github.com>
2018-01-24 10:22:43 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number to check wether positive or negative :"))
+if a>0:
+    print("the given number is positive")
+if a<0:
+    print("the given number is negative" )
---
+++ ls2.py
@@ -0,0 +1,249 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print('ritika')
+ritika
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/2.py ===================
+ritika
+>>> 5/2
+2.5
+>>> 50.2/2
+25.1
+>>> 5//2
+2
+>>> 5*2
+10
+>>> 5**2
+25
+>>> 2**6
+64
+>>> 5%2
+1
+>>> 2%5
+2
+>>> 24%5
+4
+>>> 24%4
+0
+>>> 25%4
+1
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative5
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/if.py", line 2, in <module>
+    if a>0:
+TypeError: '>' not supported between instances of 'str' and 'int'
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative5
+the given number is positive
+>>> -2
+-2
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative-2
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative2%5
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/if.py", line 1, in <module>
+    a=int(input("enter the number to check wether positive or negative"))
+ValueError: invalid literal for int() with base 10: '2%5'
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative5
+the given number is positive
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative0
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :6
+the given number is positive
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :9
+the given number is positive
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :3
+the given number is positive
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :-2
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :-5
+the given number is negative
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :6
+the given number is positive
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/if.py ===================
+enter the number to check wether positive or negative :-3
+the given number is negative
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/2.py ===================
+ritika
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/2.py", line 2, in <module>
+    if a>0:
+NameError: name 'a' is not defined
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/2.py ===================
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/2.py", line 1, in <module>
+    if a>0:
+NameError: name 'a' is not defined
+>>> 
+=================== RESTART: /home/cs2017a209/ritika/2.py ===================
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/2.py", line 1, in <module>
+    if a>0:
+NameError: name 'a' is not defined
+>>> 
+================ RESTART: /home/cs2017a209/ritika/if_else.py ================
+enter a value9
+positive
+>>> 
+================ RESTART: /home/cs2017a209/ritika/if_else.py ================
+enter a value-3
+negative
+>>> 
+================ RESTART: /home/cs2017a209/ritika/if_else.py ================
+enter a value5
+5 positive
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks25
+fail
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks56
+fail
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks95
+Grade A
+Grade B
+Grade C
+Grade D
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks95
+Grade A
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks85
+Grade B
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks75
+Grade C
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks65
+Grade D
+>>> 
+================== RESTART: /home/cs2017a209/ritika/elif.py ==================
+enter the marks40
+fail
+>>> 
+================= RESTART: /home/cs2017a209/ritika/while.py =================
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> 
+================= RESTART: /home/cs2017a209/ritika/while.py =================
+1
+11
+21
+31
+41
+51
+61
+71
+81
+91
+>>> 
+================= RESTART: /home/cs2017a209/ritika/while.py =================
+>>> 
+================= RESTART: /home/cs2017a209/ritika/while.py =================
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+>>> 
+================= RESTART: /home/cs2017a209/ritika/while.py =================
+0
+1
+2
+3
+4
+5
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+98
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+25
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+57
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+1
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+6
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+2
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+4
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+1
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+2
+>>> 
+================= RESTART: /home/cs2017a209/ritika/random.py =================
+4
+>>> 
