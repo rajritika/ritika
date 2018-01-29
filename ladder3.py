1ed8e785e13f19d85d6cc703df3ffd7ed8ef7d89
rajritika <35676563+rajritika@users.noreply.github.com>
2018-01-29 10:15:50 +0530

    Add files via upload

---
+++ ladder3.py
@@ -0,0 +1,232 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+press r to roll a dice: 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+the present value 1
+the dice value 1
+press r to roll a dice: r
+the present value 2
+the dice value 1
+press r to roll a dice: r
+the present value 4
+the dice value 2
+press r to roll a dice: r
+the present value 8
+the dice value 4
+climb: dice moves to 37
+press r to roll a dice: r
+the present value 11
+the dice value 3
+bite: dice moves to 2
+press r to roll a dice: rr
+the present value 2
+the dice value 3
+press r to roll a dice: r
+the present value 4
+the dice value 2
+press r to roll a dice: r
+the present value 6
+the dice value 2
+press r to roll a dice: r
+the present value 7
+the dice value 1
+press r to roll a dice: r
+the present value 12
+the dice value 5
+press r to roll a dice: r
+the present value 18
+the dice value 6
+press r to roll a dice: r
+the present value 20
+the dice value 2
+press r to roll a dice: r
+the present value 26
+the dice value 6
+press r to roll a dice: r
+the present value 32
+the dice value 6
+press r to roll a dice: r
+the present value 34
+the dice value 2
+press r to roll a dice: r
+the present value 36
+the dice value 2
+press r to roll a dice: r
+the present value 39
+the dice value 3
+press r to roll a dice: r
+the present value 40
+the dice value 1
+climb: dice moves to 68
+press r to roll a dice: r
+the present value 73
+the dice value 5
+press r to roll a dice: r
+the present value 79
+the dice value 6
+press r to roll a dice: r
+the present value 82
+the dice value 3
+press r to roll a dice: r
+the present value 85
+the dice value 3
+press r to roll a dice: r
+the present value 91
+the dice value 6
+press r to roll a dice: r
+the present value 96
+the dice value 5
+press r to roll a dice: r
+the present value 99
+the dice value 3
+press r to roll a dice: r
+the present value 104
+the dice value 5
+game over
+press r to roll a dice: 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+press s to roll a dice: s
+the present value 0
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/ladder.py", line 10, in <module>
+    print("the dice value",r)
+NameError: name 'r' is not defined
+>>> 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+press s to roll a dice: s
+the present value 0
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/ladder.py", line 10, in <module>
+    print("the dice value",r)
+NameError: name 'r' is not defined
+>>> 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+press s to roll a dice: s
+the present value 0
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/ladder.py", line 10, in <module>
+    print("the dice value",r)
+NameError: name 'r' is not defined
+>>> 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+Traceback (most recent call last):
+  File "/home/cs2017a209/ritika/ladder.py", line 5, in <module>
+    if (roll=='r'):
+NameError: name 'roll' is not defined
+>>> 
+================= RESTART: /home/cs2017a209/ritika/ladder.py =================
+press r to roll a dice: r
+the present value 1
+the dice value 1
+press s to roll a dice: s
+the present value 2
+the dice value 1
+press r to roll a dice: r
+the present value 4
+the dice value 2
+press s to roll a dice: s
+the present value 6
+the dice value 2
+press r to roll a dice: r
+the present value 10
+the dice value 4
+press s to roll a dice: s
+the present value 14
+the dice value 4
+press r to roll a dice: r
+the present value 16
+the dice value 2
+press s to roll a dice: s
+the present value 17
+the dice value 1
+press r to roll a dice: r
+the present value 23
+the dice value 6
+press s to roll a dice: s
+the present value 26
+the dice value 3
+press r to roll a dice: r
+the present value 27
+the dice value 1
+press s to roll a dice: s
+the present value 29
+the dice value 2
+press r to roll a dice: r
+the present value 32
+the dice value 3
+press s to roll a dice: s
+the present value 36
+the dice value 4
+press r to roll a dice: r
+the present value 39
+the dice value 3
+press s to roll a dice: s
+the present value 42
+the dice value 3
+press r to roll a dice: r
+the present value 46
+the dice value 4
+press s to roll a dice: s
+the present value 48
+the dice value 2
+press r to roll a dice: r
+the present value 51
+the dice value 3
+press s to roll a dice: s
+the present value 52
+the dice value 1
+climb: dice moves to 81
+press r to roll a dice: r
+the present value 87
+the dice value 6
+press s to roll a dice: s
+the present value 93
+the dice value 6
+bite: dice moves to 64
+press r to roll a dice: r
+the present value 68
+the dice value 4
+press s to roll a dice: s
+the present value 72
+the dice value 4
+press r to roll a dice: r
+the present value 75
+the dice value 3
+press s to roll a dice: s
+the present value 79
+the dice value 4
+press r to roll a dice: r
+the present value 82
+the dice value 3
+press s to roll a dice: s
+the present value 85
+the dice value 3
+press r to roll a dice: r
+the present value 89
+the dice value 4
+press s to roll a dice: s
+the present value 90
+the dice value 1
+press r to roll a dice: r
+the present value 91
+the dice value 1
+press s to roll a dice: s
+the present value 97
+the dice value 6
+press r to roll a dice: r
+the present value 99
+the dice value 2
+press s to roll a dice: s
+the present value 100
+the dice value 1
+game over
+press r to roll a dice: 
---
+++ ladder_lab3.py
@@ -0,0 +1,48 @@
+import random
+count=0
+while (count<=100):
+    player1=input("press r to roll a dice: ")
+    if (player1=='r'):
+        r=random.randint(1,6)
+        count=count+r
+    print("the present value",count)
+    print("the dice value",r)
+    player2=input("press s to roll a dice: ")
+    if (player2=='s'):
+        s=random.randint(1,6)
+        count=count+s
+    print("the present value",count)
+    print("the dice value",s)
+    if (count==8):
+        count=8
+        print("climb: dice moves to 37")
+    elif (count==11):
+        count=2
+        print("bite: dice moves to 2")
+    elif (count==25):
+        count=4
+        print("bite: dice moves to 4")
+    elif (count==38):
+        count=9
+        print("bite: dice moves to 9")
+    elif (count==40):
+        count=68
+        print("climb: dice moves to 68")
+    elif (count==52):
+        count=81
+        print("climb: dice moves to 81")
+    elif (count==65):
+        count=46
+        print("bite: dice moves to 46")
+    elif (count==76):
+        count=97
+        print("climb: dice moves to 97")
+    elif (count==89):
+        count=70
+        print("bite: dice moves to 70")
+    elif (count==93):
+        count=64
+        print("bite: dice moves to 64")
+    elif (count>=100):
+        count=100
+        print("wow,you won the game")
