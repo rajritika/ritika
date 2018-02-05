6bcb196ce13604944cbede11cbf2a3407455bb79
rajritika <35676563+rajritika@users.noreply.github.com>
2018-02-05 10:10:41 +0530

    Update if.py

--- if.py
+++ if.py
@@ -1,4 +1,4 @@
-a=int(input("enter the number to check wether positive or negative :"))
+a=int(input("enter the number to check wether positive or negative :")) #ritika
 if a>0:
     print("the given number is positive")
 if a<0:
