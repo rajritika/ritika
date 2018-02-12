203ad34c37b8f499b8562fcfcef24a09f975845d
rajritika <35676563+rajritika@users.noreply.github.com>
2018-02-12 09:41:28 +0530

    Update smpt.py

--- smpt.py
+++ smpt.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('ritikargar99@gmail.com', 'akitir99')
-msg = 'Hello!'
-server.sendmail('ritikargar99@gmail.com', 'sahanagowda257@gmail.com', msg)
+server.login('ritikargar99@gmail.com', 'akitir99') #login to the server
+msg = 'Hello!' #send the mail
+server.sendmail('ritikargar99@gmail.com', 'sahanagowda257@gmail.com', msg) # The /n separates the message from the headers
 server.close()
