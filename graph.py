1a42b6ed902fcd103207b15297848e9e49dbe911
rajritika <35676563+rajritika@users.noreply.github.com>
2018-03-07 10:03:32 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6]
+y=[7,8,9,1,11,12]
+plt.bar(x,y, label="bar graph")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('bar graph\n check')
+plt.legend()
+plt.show()
+
+
